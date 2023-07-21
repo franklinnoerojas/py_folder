754c8152e8d5d9adb709de45073b71dc064c27f1
franklinnoerojas <franklin.noe.pena@outlook.com>
2023-07-21 14:21:36 -0500

    update code test

--- Task_Automatization/file_mover.py
+++ Task_Automatization/file_mover.py
@@ -12,20 +12,20 @@ xlsFolder = "/Users/Usuario/Documents/Organizer/Files/"
 if __name__ == "__main__":
     for filename in os.listdir(downloadsFolder):
         name, extension = os.path.splitext(downloadsFolder + filename)
-        print(filename)
-        # if extension in [".jpg", ".jpeg", ".png"]:
-        #     os.rename(downloadsFolder + filename, imageFolder + filename)
+        #print(filename)
+        if extension in [".jpg", ".jpeg", ".png"]:
+            os.rename(downloadsFolder + filename, imageFolder + filename)
 
-        # if extension in [".mp4"]:
-        #     os.rename(downloadsFolder + filename, VideoFolder + filename)
+        if extension in [".mp4"]:
+            os.rename(downloadsFolder + filename, VideoFolder + filename)
 
-        # if extension in [".mp3"]:
-        #     os.rename(downloadsFolder + filename, AudioFolder + filename)
+        if extension in [".mp3"]:
+            os.rename(downloadsFolder + filename, AudioFolder + filename)
 
-        # if extension in [".xls",".xlsx",".csv"]:
-        #     os.rename(downloadsFolder + filename, xlsFolder + filename)
+        if extension in [".xls",".xlsx",".csv"]:
+            os.rename(downloadsFolder + filename, xlsFolder + filename)
 
-        # if extension in [".pdf"]:
-        #     os.rename(downloadsFolder + filename, pdfFolder + filename)
+        if extension in [".pdf"]:
+            os.rename(downloadsFolder + filename, pdfFolder + filename)
 
 
--- Task_Automatization/playwright_demo.py
+++ Task_Automatization/playwright_demo.py
@@ -1,29 +1,3 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
-
-
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://github.com/login")
-    page.get_by_label("Username or email address").click()
-    page.get_by_label("Username or email address").fill("franklin.noe.pena@outlook.com")
-    page.get_by_label("Username or email address").press("Tab")
-    page.get_by_label("Password").fill("AmazonessX0224.")
-    page.get_by_role("button", name="Sign in").click()
-    page.get_by_role("link", name="franklinnoerojas/play2").click()
-    page.get_by_role("link", name="Settings Settings").click()
-    page.get_by_label("Repository name").click()
-    page.get_by_label("Repository name").fill("play_demo2")
-    page.get_by_role("button", name="Rename").click()
-    page.get_by_label("Open user account menu").click()
-    page.get_by_role("link", name="Sign out").click()
-    page.get_by_role("button", name="Sign out").click()
-
-    # ---------------------
-    context.close()
-    browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
+#pip install pytest-playwright
+#playwright install
+#playwright codegen
